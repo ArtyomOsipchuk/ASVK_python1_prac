a6122386eb76563a446ea9e5216c951804ab99f2
ArtyomOsipchuk <artos4002@yandex.ru>
2024-11-24 00:18:42 +0300

    python troubles

--- 20241119/2/prog.py
+++ 20241119/2/prog.py
@@ -1,14 +1,21 @@
 class Num:
-    n = 0
+	_real = 0
 
-    def __get__(self, obj, val):
-        return self.n
-    
-    def __set__(self, obj, val):
-        try:
-            self.n = val.real
-        except:
-            self.n = val.__len__()
+        @property
+        def real(self):
+            return self._real
+
+        @real.setter
+        def real(self, val):
+            if hasattr(val, "real"):
+                self._real = val.real
+            if hasattr(val, "len"):
+                self._real = len(val)
+            return 0
+
+        @real.deleter
+        def real(self):
+            self._real = 0
 
 
 class C:
@@ -16,9 +23,11 @@ class C:
 print(C().num)
 c, d = C(), C()
 c.num = d.num = 2
+print(c.num, d.num)
 print(c.num+d.num)
 c.num = "qwerqwerqwer"
 print(c.num, d.num)
 print(c.num+d.num)
 d.num = range(10, 1000, 7)
+print(c.num, d.num)
 print(c.num+d.num)
